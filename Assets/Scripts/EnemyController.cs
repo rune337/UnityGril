c761705d33a3f7239148e5f0b135a31b6fca5363
Amadeus <>
2025-10-18 17:01:01 +0900

    追加

---
+++ Assets/Scripts/EnemyController.cs
@@ -0,0 +1,58 @@
+using UnityEngine;
+using System.Collections; // コルーチンを使うために必要
+
+public class EnemyController : MonoBehaviour
+{
+    bool isInvincible = false; // 無敵状態を表すフラグ
+    int enemyHP = 10;
+    public float invincibilityDuration = 0.5f; //無敵時間
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (isInvincible) //無敵状態なら何もしない
+            return;
+
+
+        if (other.gameObject.CompareTag("Sward"))
+        {
+            isInvincible = true; //ダメージを受けたら無敵
+            enemyHP--;
+            Debug.Log("敵のHP " + enemyHP);
+
+            //無敵時間を開始するコルーチンを呼び出す
+            StartCoroutine(SetInvincibilityTimer());
+
+        }
+
+        if (enemyHP < 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+
+    }
+
+   IEnumerator SetInvincibilityTimer()
+    {
+        //指定された時間だけ待機
+        yield return new WaitForSeconds(invincibilityDuration);
+
+        //時間が経過したら無敵状態を解除
+        isInvincible = false;
+        
+    }
+
+
+
+}
